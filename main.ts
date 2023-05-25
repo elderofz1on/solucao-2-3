35f11335f35d3224b46297bc4dc1983d4e2a6cfa
Wagner <wprodriguesbr@gmail.com>
2023-05-25 10:18:18 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Skull)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("H")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(7)
+})
+basic.forever(function () {
+	
+})
